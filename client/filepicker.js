b2398539677c5b1a9227013cc3326144993f98c8
adamgins <adam.ginsburg@buzzy-buzz.com>
2016-04-04 16:11:23 +1000

    For Debugging Meteor Filepicker on Cordova

---
+++ client/filepicker.js
@@ -0,0 +1,8 @@
+/**
+ * Created by adamginsburg on 4/04/2016.
+ */
+//import filepicker from 'filepicker-js';
+
+
+// this is equivalent to the standard node require:
+const filepicker = require('filepicker-js');
---
+++ client/main.js
@@ -0,0 +1,51 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+
+  this.counter = new ReactiveVar(0);
+});
+
+Template.hello.onRendered(function(){
+    filepicker.setKey('<PUT YOUR FILEPICKER KEY HERE>')
+});
+Template.hello.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+
+    var signature = Meteor.settings.public.filepicker.pickSignature;
+    var policy = Meteor.settings.public.filepicker.pickPolicy;
+    console.log("here 1")
+      filepicker.pickMultiple(
+        {
+          mimetype: '*/*',
+          services: ['COMPUTER', 'FACEBOOK','INSTAGRAM', 'GOOGLE_DRIVE','DROPBOX','FLICKR','PICASA','URL','WEBCAM','CONVERT'],
+          container: 'modal',
+          policy: policy,
+          signature:signature,
+          conversions: ['crop', 'rotate', 'filter']
+        },
+
+        function(Blobs){
+            console.log("here 1.1")
+          console.log("Success:", Blobs);
+
+
+        },
+        function(error){
+            console.log("here 1.2")
+         console.log(error)
+        });
+      console.log("here 2")
+
+  },
+});
